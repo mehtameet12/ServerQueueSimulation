daeed7a664343046d819c419a27d303a5718577a
Marc WU <32423253+wumarc@users.noreply.github.com>
2022-11-28 14:42:10 -0500

    Revert "Update Server.java (updated wrong branch)"
    This reverts commit 10e9e1f47de99073de429bbf8dee32d9e6d80e1b.

--- Server.java
+++ Server.java
@@ -1,63 +1,42 @@
 
 public class Server {                                           //this is the server class
-    
     private int queue;                                          //variable to keep track of how many people in its queue
     private int processing;                                 //set to true if server is busy with someone
     private int maxQueue;                                       //value to store max queue length
-    private int maxProcessor;
 
-    public Server() {
+    public Server(){
         queue = 0;
         processing = 0;
         maxQueue = 0;
-        maxProcessor = 1;
     }
-
-    public Server(int maxProcessor) {
-        queue = 0;
-        processing = 0;
-        maxQueue = 0;
-        this.maxProcessor = maxProcessor;
-    }
-
-
     private void determineMaxQueue(int queueLength){            //find the max queue length
         if(maxQueue<queueLength){
             maxQueue = queueLength;
         }
     }
-
     public boolean isBusy(){                                    //method to determine if server is busy
-        return processing==maxProcessor;
+        return processing==1;
     }
-
     public int getProcessing(){
         return processing;
     }
-
     public void addToQueue(){                                   //increase the waiting line by 1
         queue++;    
         determineMaxQueue(getQueueLength());
     }
-
     public int getQueueLength(){                                //get the current waiting line length
         return queue;
     }
-
     public void service(){                                      //set server to busy
-        processing++;
+        processing = 1;
     }
-
     public void setIdle(){                                      //set the server to idle
-        processing--;
+        processing = 0; 
     }
-
     public int getMaxQueue(){                                   //get max queue length that ever existed
         return maxQueue;
     }
-
     public void removeFromQueue(){                              //decrease the waiting line length by 1
         queue--;
     }
-
 }
